1c236f20b874fb0e2af01ff615fff064fdca6f1f
Shannon McKinney <srmckinn@gmail.com>
2016-10-02 15:56:03 -0400

    Previous and Next button functionality updates

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 $(function() {
 
 	var currentPage = 1;
-	var searchTopic, dateFrom, dateTo, orderBy;	//set all of these variables at the top so that they can be used globally
+	var searchTopic, dateFrom, dateTo, orderBy;	//set all of these variables at the top so that they can be used in other functions
 
-//API key: 9f55a2ee-1376-4d7f-b1bf-dfed2da408c5
 	$('#submit').click(function(event) {
 
 		event.preventDefault();
@@ -14,21 +13,19 @@ $(function() {
 		dateFrom = $('#from-date').val();
 		dateTo = $('#to-date').val();
 		orderBy = $('.drop-down').val();
-
-		getAjax(searchTopic, dateFrom, dateTo, orderBy, currentPage);
-
+		getAjax(searchTopic, dateFrom, dateTo, orderBy, currentPage);	//make the API call
 	});
 
-	$('#next').click(function() {
-		//go to next page
-		//location.reload();
+	function clickNext() {
 		currentPage++;
-		$('.news-results').empty();					//empty the news-results div of all elements inserted from previous searches
+		$('.news-results').empty();
 		getAjax(searchTopic, dateFrom, dateTo, orderBy, currentPage);
-	});
+	}
 
-	if (currentPage > 1) {
-		
+	function clickPrevious() {
+		currentPage--;
+		$('.news-results').empty();
+		getAjax(searchTopic, dateFrom, dateTo, orderBy, currentPage);
 	}
 
 	function getAjax(searchTopic, dateFrom, dateTo, orderBy, currentPage) {
@@ -36,13 +33,25 @@ $(function() {
 			url: 'https://content.guardianapis.com/search?page=' + currentPage + '&q=' + searchTopic + '&api-key=9f55a2ee-1376-4d7f-b1bf-dfed2da408c5' + '&from-date=' + dateFrom + '&to-date=' + dateTo + '&order-by=' + orderBy,
 			success: function (data) {
 
+				//below conditional statements disable/enable Previous & Next buttons and make them unclickable/clickable depending on page number
+				if(currentPage === data.response.pages) {
+					$('.next').addClass('disabled').off('click');
+				} else {
+					$('.next').removeClass('disabled').on('click', clickNext);
+				}
+
+				if(currentPage === 1) {
+					$('.previous').addClass('disabled').off('click');
+				} else {
+					$('.previous').removeClass('disabled').on('click', clickPrevious);
+				}
+
 				//for each search result, create a link to the news story along with the date.
 				//regEx to extract the part of the date that we actually want to display.
 				data.response.results.forEach(function (element, index, array) {
 					var contentDate = element.webPublicationDate.match(/\d{4}[-]\d{2}[-]\d{2}/g);
 					var contentHtml = '<div class="content-html hidden">' + '<a href="' + element.webUrl + '">' + element.webTitle + '</a>' + '<br>' + contentDate + '</div>';
 					$('.news-results').append(contentHtml);
-					//$('.news-results').text(element.webTitle);
 				})
 				
 				$(".content-html").removeClass('hidden');	//the "hidden" class causes everything from forEach to show at the same time. Remove later?
@@ -55,9 +64,5 @@ $(function() {
  		
 		});
 	}
-
-/*	function nextPage(pageNumber) {
-
-	}*/
 
 });
